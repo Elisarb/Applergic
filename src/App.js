f23ddd4a0db0e057e9053fc50f35b86fca7663b2
yayo.262002 <carlosmarchan2602@gmail.com>
2022-11-23 12:18:35 +0100

    aaa

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <p> HOLA </p>
+          <p> HOLA juan</p>
           Learn React
         </a>
       </header>
