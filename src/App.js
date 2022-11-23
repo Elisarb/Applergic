a9b8433fbc2f49bb3a0e3d5d0bd483862c2fdf72
yayo.262002 <carlosmarchan2602@gmail.com>
2022-11-23 17:36:37 +0100

    eee

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Link, Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import Bienvenida from './Components/Pages/Bienvenida/Bienvenida';
 import Portada from './Components/Pages/Portada/Portada';
 import Registro from './Components/Pages/Registro/Registro';
+import RegistroSos from './Components/Pages/RegistroSos/RegistroSos';
+
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
           <Route path="/" element={<Bienvenida></Bienvenida>} />
           <Route path="/Portada" element={<Portada></Portada>} />
           <Route path="/Registro" element={<Registro></Registro>} />
+          <Route path="/Emergencias" element={<RegistroSos></RegistroSos>} />
         </Routes>
     </Router>
    
