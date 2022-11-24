1e4b1a200611b2c65ae1754d343b21bd751787ca
DarkMatt12 <novamatt12@gmail.com>
2022-11-24 09:58:27 +0100

    cambio de fav

--- src/Components/Pages/Favoritos/Favoritos.jsx
+++ src/Components/Pages/Favoritos/Favoritos.jsx
@@ -1,8 +1,20 @@
 import Img1 from '../../../Assets/Fav-Prueba.png'
+import BtnDiary from '../../Componentes/Btn-Diary/Btn-Diary'
+import BtnFav from '../../Componentes/Btn-Fav/Btn-Fav'
+import BtnHome from '../../Componentes/Btn-Home/Btn-Home'
+import BtnShare from '../../Componentes/Btn-Share/Btn-Share'
 const Fav = () => {
     return (
       <div>
         <img src={Img1} alt="" />
+        <div className='Home-Footer'>
+            <div className='Home-Footer-Caja'>
+                <BtnHome></BtnHome>
+                <BtnFav></BtnFav>
+                <BtnDiary></BtnDiary>
+                <BtnShare></BtnShare>
+            </div>
+        </div>
       </div>
     )
   }
